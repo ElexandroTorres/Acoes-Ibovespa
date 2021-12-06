8a560492086fa1aff8430a9a0e60ac9f1cb290d0
Elexandro Torres <elexandro.torres@gmail.com>
2021-12-06 11:12:00 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/acoes_ibovespa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.acoes_ibovespa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => AcoesIbovespa();
+
+class AcoesIbovespa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
